916989b5502f46b00bd61b72c1c5319ca8a1e8ae
IhorMtr <igormotornyyy@gmail.com>
2024-12-23 01:09:58 +0200

    Prevent body scroll when mobile menu is open
    Added `no-scroll` class to disable body scrolling when the mobile menu is opened and removed it when closed. Updated relevant styles in CSS and JavaScript to support this behavior.

--- scripts/script.js
+++ scripts/script.js
@@ -7,12 +7,15 @@ document.getElementById('modal-closer').addEventListener('click', function () {
     const modalOverlay = document.getElementById('modal-overlay');
     modalOverlay.classList.remove('is-open');
 });
+
 document.getElementById('mobile-menu-opener').addEventListener('click', function () {
     const mobileMenu = document.getElementById('mobile-menu');
     mobileMenu.classList.add('is-open');
+    document.body.classList.add('no-scroll');
 });
 
 document.getElementById('mobile-menu-closer').addEventListener('click', function () {
     const mobileMenu = document.getElementById('mobile-menu');
     mobileMenu.classList.remove('is-open');
+    document.body.classList.remove('no-scroll');
 });
