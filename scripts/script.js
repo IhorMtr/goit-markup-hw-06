b8fa82227a70db92e45367c961123d9e697bcbb0
IhorMtr <igormotornyyy@gmail.com>
2024-12-23 01:02:23 +0200

    Refactor CSS and HTML for social item classes and structure
    Updated social item class names in HTML for consistency and improved maintainability. Refined CSS styles, added new utility classes (e.g., `.visible`), and normalized `line-height` values for more clarity. Enhanced mobile menu structure and organized base/global styles for better responsiveness.

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,12 @@ document.getElementById('modal-closer').addEventListener('click', function () {
     const modalOverlay = document.getElementById('modal-overlay');
     modalOverlay.classList.remove('is-open');
 });
+document.getElementById('mobile-menu-opener').addEventListener('click', function () {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.add('is-open');
+});
+
+document.getElementById('mobile-menu-closer').addEventListener('click', function () {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.remove('is-open');
+});
